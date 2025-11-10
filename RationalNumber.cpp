e307f0ea5b70e14da40b706597ee3718b603af9d
MetelAA <artemmetelski78@gmail.com>
2025-11-10 23:20:45 +0300

    Q7,Q8 added

--- RationalNumber.cpp
+++ RationalNumber.cpp
@@ -157,3 +157,34 @@ RationalNumber RationalNumber::subtract(const RationalNumber &other) const {
     return RationalNumber(diffOfNumerator, commonDenominator);//хороший вопрос может ли здесь возникнуть ситуация с ведущими нулями, но вроде не может, тк ответсвенность за уничтожение ведущих нулей на IntegerNumber::subtract
 }
 
+//Q-7 Умножение рациональных чисел
+RationalNumber RationalNumber::multiply(const RationalNumber& other) const {
+    //Пользуемся умножением натуральных и целых чисел
+    IntegerNumber intres(this->numerator->multiply(other.getIntegerNumerator()));
+    NaturalNumber natres(this->denominator->multiply(other.getNaturalDenominator()));
+    //Сокращаем полученную дробь
+    RationalNumber result(intres, natres);
+    result.reduce();
+    return result;
+}
+
+//Q-8 Деление рациональных чисел
+RationalNumber RationalNumber::division(const RationalNumber& other) const {
+    //Проверка на делитель равный 0
+    if (!other.getIntegerNumerator().getSign()) {
+        throw UniversalStringException("you can't divide by zero");
+    }
+
+    //Получаем знак результата
+    bool ressign = other.getIntegerNumerator().isNegative();
+    //Создаём удобные для дальнейших вычислений объекты
+    IntegerNumber firstmul(other.getNaturalDenominator().getNumbers(), ressign);
+    NaturalNumber secondmul(other.getIntegerNumerator().getNumbers());
+    //Так как деление это умножение на обратную дробь, применяем методы умножения
+    IntegerNumber intres(this->numerator->multiply(firstmul));
+    NaturalNumber natres(this->denominator->multiply(secondmul));
+    //Сокращение дроби
+    RationalNumber result(intres, natres);
+    result.reduce();
+    return result;
+}
